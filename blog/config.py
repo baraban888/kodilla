49eb05350ec79e2718c7133c6311de234f0e24a5
baraban888 <1975bahat@gmail.com>
2025-11-24 21:21:12 +0100

    Add login,haslo

--- blog/config.py
+++ blog/config.py
@@ -4,3 +4,5 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
+    ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "change-me")
--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, BooleanField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, TextAreaField, BooleanField, PasswordField
+from wtforms.validators import DataRequired, ValidationError
+from blog.config import Config
 
 class EntryForm(FlaskForm):
     title = StringField(
@@ -12,3 +13,15 @@ class EntryForm(FlaskForm):
         validators=[DataRequired()]
     )
     is_published = BooleanField('Wpis opublikowany')
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+
+    def validate_username(self, field):
+        if field.data != Config.ADMIN_USERNAME:
+            raise ValidationError("Invalid username")
+
+    def validate_password(self, field):
+        if field.data != Config.ADMIN_PASSWORD:
+            raise ValidationError("Invalid password")
